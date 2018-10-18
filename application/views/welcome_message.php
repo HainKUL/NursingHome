9ce3ca9f91b8f127eb49f8b2fd7036ce465c4457
michael.penninck <michael.penninck@student.kuleuven.be>
2018-10-18 17:11:47 +0200

    Colour change

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}
 
 	h1 {
-		color: #444;
+		color: #445;
 		background-color: transparent;
 		border-bottom: 1px solid #D0D0D0;
 		font-size: 19px;
