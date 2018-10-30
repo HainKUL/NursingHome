16e631a30fc7e1d50907ae9283b6964b49f00795
jan.buskens <jan.buskens@student.kuleuven.be>
2018-10-30 15:18:19 +0100

    Changed to default controller from the welcome screen to our own homepage

--- application/controllers/Homepage_controller.php
+++ application/controllers/Homepage_controller.php
@@ -19,4 +19,23 @@ class Homepage_controller extends CI_Controller{
         $this->parser->parse('Homepage_view', $data);
     }
 
+    public function residentHome(){
+        $data['page_title'] = 'UXWD home page';
+        $data['content_title_1'] = 'residents homepage';
+        $data['content_title_2'] = 'Welcome to the residents homepage';
+        $data['content'] = 'content';
+        $this->parser->parse('Homepage_view', $data);
+    }
+
+    public function caregiverHome(){
+        $this->load->model('DBdump_model');
+        $oldPeople = $this->DBdump_model->get_oldPeople();
+        $data['page_title'] = 'UXWD home page';
+        $data['content_title_1'] = 'caregiver homepage';
+        $data['content_title_2'] = 'database test';
+        $data2['oldPeople'] = $oldPeople;
+        $data['content'] = $this->parser->parse('oldPeople', $data2, true);
+        $this->parser->parse('Homepage_view', $data);
+    }
+
 }
