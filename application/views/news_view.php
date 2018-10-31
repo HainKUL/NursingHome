3ac5680e3957900a2fcf6b33f6ba2e0b0dfad7da
hanne.croonen <hanne.croonen@student.kuleuven.be>
2018-10-31 18:46:51 +0100

    NEWS page added (without layout)

---
+++ application/views/news_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{page_title}</title>
+    <link href="<?=base_url() ?>assets/css/main.css" rel="stylesheet" type="text/css"/>
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="UXWD's course demo" />
+
+</head>
+    <h4>Het Laatste Nieuws</h4>
+    <button type="button" ><a href="https://www.hln.be">KLIK HIER</a></button>
+    <h4>Het Nieuwsblad </h4>
+    <button type="button" ><a href="https://www.nieuwsblad.be">KLIK HIER</a></button>
+    <h4>De Standaard </h4>
+    <button type="button" ><a href="https://www.standaard.be">KLIK HIER</a></button>
+    <h4>De Morgen</h4>
+    <button type="button" ><a href="https://www.demorgen.be">KLIK HIER</a></button>
+</html>
+
