9313129eb71f451f26268437ed912408f5e7745a
michael.penninck <michael.penninck@student.kuleuven.be>
2018-11-05 20:27:46 +0100

    done screen added in new controller added forgot password screen

---
+++ application/views/password_forgot.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<html>
+<head>
+    <title>{page_title}</title>
+    <link href="http://localhost/a18ux04/assets/css/password_forgot.css" rel="stylesheet" type="text/css"/>
+    <!-- <link href="<?=base_url() ?>assets/css/password_forgot.css" rel="stylesheet" type="text/css"/> for on the actual site-->
+</head>
+
+
+<body>
+<div id="title">
+    <p>{head_message}</p>
+</div>
+
+<div id="text">
+    <p>{first_sentence}</p>
+    <p> E-mail: <input type="email" name="emailaddress" id = "emailInput" placeholder="e-mail"> </p>
+</div>
+
+<div id="button">
+    {button_text}
+</div>
+
+</body>
+
+<footer>
+    <p>Copyright © 2018 UXWD. Groep T All Rights Reserved.
+        <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a>
+    </p>
+</footer>
+
+</html>
