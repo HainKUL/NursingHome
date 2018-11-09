18d73bc39e182c1bea39d97070350dc2af22a0d7
haien.tang <haien.tang@student.kuleuven.be>
2018-11-09 11:17:28 +0100

    up-to-data

--- application/models/Question_model.php
+++ application/models/Question_model.php
@@ -8,10 +8,20 @@
 
 
 class Question_model extends CI_Model{
+//    public function get_questions(){
+//        $query=$this->db->get("Questions");//table name
+//
+//        return $query->result();
+//    }
     public function get_questions(){
-        //$this->db->order_by("date","desc");
-        $query=$this->db->get("Question");//table name
-        return $query->result();
-    }
+        $query=$this->db->query("SELECT * FROM a18ux04.Questions WHERE idQuestions=1;");//select only one colume for test here
+        foreach ($query->result_array() as $row)
+        {
+            $data['question'] = $row['question'];
+            $data['category'] = $row['category'];
+            $data['progress'] = $row['idQuestions'] . "/52";
+        }
 
+        return $data;
+    }
 }
