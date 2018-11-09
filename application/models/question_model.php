e8cdfaf9673e7b25948c022fb647a13c6d2eafcc
jan.buskens <janbmx@gmail.com>
2018-11-09 08:12:49 +0100

    updated menu_model

--- application/models/question_model.php
+++ application/models/question_model.php
@@ -9,14 +9,9 @@
 
 class Question_model extends CI_Model{
     public function get_questions(){
-        $query=$this->db->query("SELECT * FROM a18ux04.Questions WHERE idQuestions=1;");//select only one colume for test here
-        foreach ($query->result_array() as $row)
-        {
-            $data['question'] = $row['question'];
-            $data['category'] = $row['category'];
-            $data['progress'] = $row['idQuestions'] . "/52";
-        }
-
-        return $data;
+        //$this->db->order_by("date","desc");
+        $query=$this->db->get("Question");//table name
+        return $query->result();
     }
+
 }
