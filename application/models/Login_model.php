36a0b8d97c5b32a475629fc02f9f6d8a3041db84
weihao.yan <weihao.yan@student.kuleuven.be>
2018-11-15 23:12:52 +0100

    autoload changed

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -12,6 +12,7 @@ class Login_model extends CI_Model
     function __construct() {
         parent::__construct();
         $this->load->database();
+        $this->load->library('session');
     }
     public function validate_user($data) {
         $this->db->where('email', $data['email']);
