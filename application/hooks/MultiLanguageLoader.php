cd3461663b1b83a563e17d29257417dd32006f06
weihao <weihao.yan@student.kuleuven.be>
2018-11-20 14:42:31 +0100

    UPDATE multi-language function (working in homepage, dashboard page, login page)

--- application/hooks/MultiLanguageLoader.php
+++ application/hooks/MultiLanguageLoader.php
@@ -9,8 +9,6 @@ class MultiLanguageLoader
         $ci->load->helper('language');
         $siteLang = $ci->session->userdata('site_lang');
         if ($siteLang) {
-            $ci->lang->load('header',$siteLang);
-            $ci->lang->load('content',$siteLang);
             $ci->lang->load('footer',$siteLang);
             $ci->lang->load('login',$siteLang);
             $ci->lang->load('dashboard',$siteLang);
@@ -18,8 +16,6 @@ class MultiLanguageLoader
 
         } else {
             // default language files
-            $ci->lang->load('header','english');
-            $ci->lang->load('content','english');
             $ci->lang->load('footer','english');
             $ci->lang->load('login','english');
             $ci->lang->load('dashboard','english');
