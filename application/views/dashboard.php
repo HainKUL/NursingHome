22e6c1d171438b0258c7fddb346dfa09cddecba3
Hanne <Hanne.croonen@hotmail.Com>
2018-11-22 11:01:15 +0100

    weather
    Signed-off-by: Hanne <Hanne.croonen@hotmail.Com>

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -183,6 +183,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<a href="<?= site_url('Caregiver_controller/login') ?>">Logout</a>
+<button type="button"><a href="localhost:8888/a18ux04/index.php/Caregiver_controller/login">Logout</button></a>
 </body>
 </html>
--- application/views/weathers.php
+++ application/views/weathers.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-    <meta name="keywords" content="UXWD's course demo" />
     <title>{page_title}</title>
 
 </head>
@@ -31,10 +30,13 @@
 </style>
 
 <body>
-<p class ="titel1">{content_title_1}</p>
 <button type="button"class="button1" ><a class="link1" href="residentHome">{buttonBack}</a></button>
 
 <!--<iframe scrolling="no" width="334" height ="175" frameborder="0" marginwidth="0" marginheight="0" src="https://www.meteo.be/services/widget/.?postcode=3000&nbDay=2&type=4&lang=nl&bgImageId=1&bgColor=567cd2&scrolChoice=0&colorTempMax=A5D6FF&colorTempMin=ffffff"></iframe>-->
-<iframe scrolling="no" width="1950" height ="700" frameborder="0" marginwidth="0" marginheight="0" src="https://www.meteo.be/services/widget/.?postcode=3000&nbDay=2&type=11&lang=nl&bgImageId=1&bgColor=567cd2&scrolChoice=2&colorTempMax=ffffff&colorTempMin=ffffff"></iframe>
+<iframe scrolling="no" width="1800" height ="900" frameborder="0" marginwidth="0" marginheight="0" src="https://www.meteo.be/services/widget/.?postcode=3000&nbDay=2&type=11&lang=nl&bgImageId=1&bgColor=567cd2&scrolChoice=0&colorTempMax=ffffff&colorTempMin=ffffff"></iframe>
+<!--<script src="//www.powr.io/powr.js?external-type=html"></script>
+<div class="powr-weather" id="2ca6ab20_1542708558"></div>-->
+
+
 </body>
 </html>
