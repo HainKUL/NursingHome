89f02cc6698697f5e98b046f6a271ec75a34da4f
Hanne <Hanne.croonen@hotmail.Com>
2018-11-30 14:38:24 +0100

    questionnaire layout
    Signed-off-by: Hanne <Hanne.croonen@hotmail.Com>

--- application/views/questionnaire.php
+++ application/views/questionnaire.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> <?php echo $this->lang->line('title'); ?></title>
+    <title> Questionnaire</title>
     <link rel="icon" href="<?=base_url()?>assets/photos/favicon.png" type="image/gif" sizes="16x16">
 
     <link href="<?= base_url()?>assets/css/features.css" rel="stylesheet" type="text/css"/>
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <!-- Custom CSS one for localhost, the other for on the server-->
-    <link href="<?=base_url()?>assets/css/older_adult.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/css/features.css" rel="stylesheet" type="text/css"/>
 
     <?php if(isset($jslibs_to_load)) foreach ($jslibs_to_load as $jslib) : ?>
         <script src="<?= base_url()?>assets/js/<?=$jslib?>"></script>
@@ -42,15 +42,15 @@
 
 <body>
 
-<div class="container-fluid ">
+<div class="container card ">
 
     <div id="card">
 
-        <div class="row " id="top_row">
+        <div class="row" >
             <div class="col-8">
-                <div id="category">
+                <h1>
                     <p>{category}</p>
-                </div>
+                </h1>
             </div>
             <div class="col-3">
             </div>
@@ -78,44 +78,44 @@
                 </div>
             </div>
             -->
-            <div class="col-12">
-                <div id="text">
+            <div class="col-12" style="text-align: center">
+                <h2>
                     <p>{question}</p>
-                </div>
+                </h2>
             </div>
         </div>
 
-        <div class="row" id="fourth_row">
+        <div class="row" style="padding-top: 10%" >
 
             <div class="col-1">
             </div>
             <div class="col-2">
                 <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
-                    <input type="submit" name="never" value="never" id="never" class="answer_button">
+                    <input type="submit" name="never" value="never" id="never" class="button2">
                 </form>
             </div>
 
             <div class="col-2">
                 <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
-                    <input type="submit" name="rarely" value="rarely" id="rarely" class="answer_button">
+                    <input type="submit" name="rarely" value="rarely" id="rarely" class="button2">
                 </form>
             </div>
 
             <div class="col-2">
                 <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
-                    <input type="submit" name="sometimes" value="sometimes" id="sometime" class="answer_button">
+                    <input type="submit" name="sometimes" value="sometimes" id="sometime" class="button2">
                 </form>
             </div>
 
             <div class="col-2">
                 <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
-                    <input type="submit" name="mostly" value="mostly" id="mostly" class="answer_button">
+                    <input type="submit" name="mostly" value="mostly" id="mostly" class="button2">
                 </form>
             </div>
 
             <div class="col-2">
                 <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
-                    <input type="submit" name="always" value="always" id="always" class="answer_button">
+                    <input type="submit" name="always" value="always" id="always" class="button2">
                 </form>
             </div>
 
@@ -124,7 +124,7 @@
 
         </div>
 
-        <div class="row" id="fifth_row">
+        <div class="row" style="padding-top: 10%">
 
             <div class="col-12">
                 <div id="progress">
