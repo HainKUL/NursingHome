7208bdd3d54e33117d461d65352a7fe8a3aa8045
Hanne <Hanne.croonen@hotmail.Com>
2018-12-05 22:08:40 +0100

    logout button
    Signed-off-by: Hanne <Hanne.croonen@hotmail.Com>

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -54,7 +54,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="<?=base_url()?>assets/photos/favicon.png" type="image/gif" sizes="16x16">
+    <link rel="icon" href="<?=base_url()?>assets/photos/favicon3.png" type="image/gif" sizes="16x16">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -82,6 +82,9 @@ $residents = $this->db->query($query);
 
     <div class="row" style="height:100vh;">
         <div class="col-3" style="background-color:#009489;padding:0;">
+            <button class="btn btn-primary btn-lg" type="button" style="min-width:100%;background-color:#009489;border:none;">
+                <a href="<?= site_url('Caregiver_controller/login') ?>"><p><?php echo $this->lang->line('dash_logout'); ?><?php unset($_SESSION['username']); ?></p></a>
+            </button>
             <div style="height:5%;"></div>
             <div class="searchdiv" style="text-align:center;margin:15px;">
                 <h2 class="floornumber"><?php echo $this->lang->line('dash_floor'); ?> 1</h2><input type="search" placeholder="<?php echo $this->lang->line('search'); ?>" style="width:100%;height:40px;"></div>
@@ -354,7 +357,6 @@ $residents = $this->db->query($query);
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<a href="<?= site_url('Caregiver_controller/login') ?>"><p><?php echo $this->lang->line('dash_logout'); ?><?php unset($_SESSION['username']); ?></p></a>
 </body>
 
 
