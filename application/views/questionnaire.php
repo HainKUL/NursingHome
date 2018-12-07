e304fbd3eeffedfb165e443a529c83e76f005e2c
martijn.hellemans <martijn.hellemans@student.kuleuven.be>
2018-12-07 14:21:47 +0100

    Enable previous question in questionnaire
    Signed-off-by: martijn.hellemans <martijn.hellemans@student.kuleuven.be>

--- application/views/questionnaire.php
+++ application/views/questionnaire.php
@@ -138,10 +138,10 @@
 
 
 
-            <div class="col-1">
-                <div id="return">
-                    <a href=""> <?php echo $this->lang->line('Return'); ?></a>
-                </div>
+            <div class="col-2">
+                <form action="<?= site_url('questionnaire_controller/update') ?>" method="get">
+                    <input type="submit" name=<?php echo $this->lang->line('Return');?> value="previous" id="previous" class="button2">
+                </form>
                 <div class="col-11">
                 </div>
             </div>
