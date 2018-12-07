514d3533d16ca0bf41ef4ff64ec0b57d7ee9b37e
Hanne <Hanne.croonen@hotmail.Com>
2018-12-07 15:36:30 +0100

    smiley fix (sorry)
    Signed-off-by: Hanne <Hanne.croonen@hotmail.Com>

--- application/language/english/done_lang.php
+++ application/language/english/done_lang.php
@@ -5,7 +5,7 @@
  * Date: 07/12/2018
  * Time: 14:28
  */
-$lang['well_done'] = 'Well done! :)';
+$lang['well_done'] = 'Well done! 😊';
 $lang['filled'] = 'You have filled in every question';
 $lang['click'] = 'Click on the button below to go back to the homescreen';
 $lang['button'] = 'Click here';
