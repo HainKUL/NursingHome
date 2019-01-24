6db7e72a523d31d035e72f9ef7a33dfb9c54c633
jan.buskens <jan.buskens@student.kuleuven.be>
2019-01-24 18:51:33 +0100

    weather iFrame scaled to fit screen

--- application/views/weathers.php
+++ application/views/weathers.php
@@ -22,6 +22,7 @@ if(!isset($_SESSION["resident"]))
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="<?=base_url() ?>assets/css/questionnaire.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url() ?>assets/css/weather.css" rel="stylesheet" type="text/css"/>
     <!-- language changer -->
 
 </head>
@@ -44,7 +45,7 @@ if(!isset($_SESSION["resident"]))
     </div>
     <div class="row" >
         <div class="col-md-0" style="left: 0" >
-            <iframe scrolling="no" width="1800" height ="700" frameborder="0" marginwidth="0" marginheight="0" src="https://www.meteo.be/services/widget/.?postcode=3000&nbDay=2&type=11&lang=nl&bgImageId=1&bgColor=567cd2&scrolChoice=0&colorTempMax=ffffff&colorTempMin=ffffff"></iframe>
+            <iframe class="frame" scrolling="no" width="1800" height ="700" frameborder="0" marginwidth="0" marginheight="0" src="https://www.meteo.be/services/widget/.?postcode=3000&nbDay=2&type=11&lang=nl&bgImageId=1&bgColor=567cd2&scrolChoice=0&colorTempMax=ffffff&colorTempMin=ffffff"></iframe>
         </div>
     </div>
 
