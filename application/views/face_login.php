a3182b8f01469d9d4e160cfb3faf63072c6ddc29
Hanne <Hanne.croonen@hotmail.Com>
2019-01-24 10:35:31 +0100

    layout facelogin
    Signed-off-by: Hanne <Hanne.croonen@hotmail.Com>

--- application/views/face_login.php
+++ application/views/face_login.php
@@ -4,87 +4,110 @@
     <meta charset="UTF-8">
     <title>Camera</title>
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="../../assets/css/features.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="../../assets/css/questionnaire.css"  media="screen,projection"/>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="../../assets/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
-<!--<body onload="init();">-->
 <body onload="startWebcam()">
-<!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="../../assets/js/materialize.min.js"></script>
-<nav>
-    <div class="nav-wrapper">
-        <h1><?php echo $this->lang->line('login_welcome'); ?></h1>
+<div class="container-fluid menu_container">
+    <div class="row header " >
+        <div class="col-5"></div>
+        <div class="col-4 header-title">
+            <p><?php echo $this->lang->line('login_welcome'); ?></p>
+        </div>
 
+        <div class="col-3"></div>
     </div>
-</nav>
-
-
-<div class="container">
-    <div class="card center">
-        <div class="card-content">
-            <div class="card-action">
-                <div class="card-action">
-
-                    <table align="center" cellpadding = "10">
-                        <tr>
-                            <h2 align="center"><?php echo $this->lang->line('login_face'); ?></h2>
-                        </tr>
-                        <tr>
-<!--                            <td>-->
-<!--                                <button class="button_logout" onclick="startWebcam();">--><?php //echo $this->lang->line('login_start'); ?><!--</button>-->
-<!--                            </td>-->
-                            <td>
-                                <button class="button_logout" onclick="snapshot();"><?php echo $this->lang->line('login_capture'); ?></button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-
-            <div>
+    <div class = "col-12 info">
+        <?php echo $this->lang->line('login_face'); ?>
+    </div>
+    <div class="row">
+        <div class=" col-4"></div>
+        <div class = "col-4 ">
+            <button type = "button" class="button_picture" onclick="snapshot();"><?php echo $this->lang->line('login_capture'); ?></button>
+        </div>
+        <div class = "col-4"></div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8 button-box">
+            <div class="row" >
+                <div class="col-1"></div>
                 <video onclick="snapshot(this)" id="video" width="400" height="300" controls autoplay></video>
                 <canvas  id="myCanvas" width="400" height="300"></canvas>
             </div>
-        </div>
-        <!--<audio id="myAudio">-->
-        <!--<source src="success.wav" type="audio/wav">-->
-        <!--</audio>-->
 
+        <div class="col-12 alternative"><?php echo $this->lang->line('alternative'); ?></div>
+
+        <form method="post" action="<?=base_url()?>Homepage_controller/login">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-2"><label><?php echo $this->lang->line('register_firstname'); ?>: </label>
+                </div>
+                <div class="col-4">
+                    <input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="firstname" id="firstname" class="form-control"  />
+
+                </div>
+            </div>
+
+            <div class="row"style="padding-top: 2vh">
+                <div class="col-3"></div>
+                <div class="col-2"><label><?php echo $this->lang->line('register_lastname'); ?>: </label>
+                </div>
+                <div class="col-4">
+                    <input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="firstname" id="firstname" class="form-control"  />
+
+                </div>
+            </div>
+            <div class="row" style="padding-top: 2vh">
+                <div class="col-3"></div>
+                <div class="col-2"><label>Pincode: </label>
+                </div>
+                <div class="col-4">
+                    <input type="password" oninput="checkInput(this.id)" pattern="[0-9]{1,4}" name="pincode" id="pincode" class="form-control"  />
+                </div>
+            </div>
+            <div class="row"style="padding-top: 2vh">
+                <div class="col-3"></div>
+                <div class ="col-6">
+                <input class="button_picture" type="submit" id="submit" value="<?php echo $this->lang->line('login_button'); ?>" />
+                </div>
+            </div>
+           <!-- <table align="center" cellpadding = "10">
+
+            <tr>
+                <td><label><?php echo $this->lang->line('register_firstname'); ?>: </label></td>
+                <td><input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="firstname" id="firstname" class="form-control"  /></td>
+            </tr>
+            <tr>
+
+                <td><label><?php echo $this->lang->line('register_lastname'); ?>: </label></td>
+                <td><input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="name" id="name" class="form-control"  /></td>
+            </tr>
+            <tr>
+
+                <td><label>Pincode:</label></td>
+                <td><input type="password" oninput="checkInput(this.id)" pattern="[0-9]{1,4}" name="pincode" id="pincode" class="form-control"  /></td>
+            </tr>
+
+            <tr>
+                <td>
+
+                     <input class="button_picture" type="submit" id="submit" value="<?php echo $this->lang->line('login_button'); ?>" />
+
+                </td>
+            </tr>
+            </table>-->
+        </form>
+        </div>
     </div>
 </div>
-<form method="post" action="<?=base_url()?>Homepage_controller/login">
-
-    <table align="center" cellpadding = "10">
-        <tr>
-            <h2 align="center"><?php echo $this->lang->line('alternative'); ?></h2>
-        </tr>
-    <tr>
-        <td><label><?php echo $this->lang->line('register_firstname'); ?>: </label></td>
-        <td><input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="firstname" id="firstname" class="form-control"  /></td>
-    </tr>
-    <tr>
-
-        <td><label><?php echo $this->lang->line('register_lastname'); ?>: </label></td>
-        <td><input type="text" oninput="checkInput(this.id)" pattern="[a-z A-Z'éèëï-]{1,20}" name="name" id="name" class="form-control"  /></td>
-    </tr>
-    <tr>
-
-        <td><label>Pincode:</label></td>
-        <td><input type="password" oninput="checkInput(this.id)" pattern="[0-9]{1,4}" name="pincode" id="pincode" class="form-control"  /></td>
-    </tr>
-
-    <tr>
-        <td>
-
-             <input class="button_logout" type="submit" id="submit" value="<?php echo $this->lang->line('login_button'); ?>" />
-
-        </td>
-    </tr>
-    </table>
 </body>
 <script src="../../assets/js/face_login.js"></script>
 <script>
